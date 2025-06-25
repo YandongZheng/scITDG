761f20a2e5898dfd7685206742c3737d6ab58fa8
YandongZheng <46453947+YandongZheng@users.noreply.github.com>
2025-06-25 18:16:20 +0800

    Update calVurveApply.R

--- R/calVurveApply.R
+++ R/calVurveApply.R
@@ -8,16 +8,30 @@
 #' @export
 #'
 #' @examples
+
 calVurveApply <- function(X, FUN, ...) {
+  # 获取 phenoData
+  pdata <- pData(X)
+  
+  # 创建数据列表
+  data_list <- as.list(pdata)
+  
+  # 创建新环境（父环境为调用环境）
   e <- new.env(parent = environment(FUN))
   
-  pdata <- pData(X)
-  for(n in names(pdata)) {
-    assign(n, pdata[[n]], envir = e)
-  }
+  # 将数据添加到新环境
+  list2env(data_list, envir = e)
   
+  # 设置函数环境
   environment(FUN) <- e
-  res <- apply(X = exprs(X), MARGIN = 1, FUN, ...)
-  names(res) <- row.names(X)
-  res
+  
+  # 应用函数
+  res <- apply(X = exprs(X), MARGIN = 1, function(gene_expr) {
+    # 将基因表达数据添加到环境
+    e$gene_expr <- gene_expr
+    FUN(gene_expr, ...)
+  })
+  
+  names(res) <- rownames(X)
+  return(res)
 }
