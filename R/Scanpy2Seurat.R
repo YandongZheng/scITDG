a34058f24e3d99dec2903f91ee8c96a71051469a
YandongZheng <46453947+YandongZheng@users.noreply.github.com>
2025-07-22 09:31:24 +0800

    Update Scanpy2Seurat.R

--- R/Scanpy2Seurat.R
+++ R/Scanpy2Seurat.R
@@ -15,35 +15,30 @@ Scanpy2Seurat <- function(file.name, data.type) {
     stop("Please input data.type!")
   }
 
-  adata = scanpy$read_h5ad(file.name) ###载入scanpy输出的h5ad文件
+  adata = scanpy$read_h5ad(file.name) 
 
-  #######导出基因名和样本信息################
-  # .obs存储meta信息，.var存储基因信息
   meta = adata$obs
   gene <- adata$var
 
-  #############导出矩阵并转置，scanpy和Seurat的行列是反的#############
-  # .X存储矩阵信息，.T存储的是转置信息，.T$X为转置矩阵
-
-  # Normalized data
+  # output Normalized data
   mtx.data <- tryCatch({
     mtx <- pandas$DataFrame(data = adata$T$X$todense(),
-                            index = adata$var_names, #行名
-                            columns = adata$obs_names) # 列名
+                            index = adata$var_names, 
+                            columns = adata$obs_names) 
     return(mtx)
   }, error = function(e) {
     mtx <- pandas$DataFrame(data = adata$T$X %>% as.matrix,
-                            index = adata$var_names, #行名
-                            columns = adata$obs_names) # 列名
+                            index = adata$var_names, 
+                            columns = adata$obs_names) 
     return(mtx)
   })
 
 
-  # raw count
+  # output raw count
   mtx.count <- tryCatch({
     mtx <- pandas$DataFrame(data = adata$raw$X$todense() %>% t,
-                            index = adata$raw$var_names, #行名
-                            columns = adata$raw$obs_names) # 列名
+                            index = adata$raw$var_names, 
+                            columns = adata$raw$obs_names) 
     return(mtx)
   }, error = function(e) {
     mtx <- pandas$DataFrame(data = adata$raw$X %>% as.matrix %>% t,
@@ -51,11 +46,6 @@ Scanpy2Seurat <- function(file.name, data.type) {
                             columns = adata$raw$obs_names) # 列名
     return(mtx)
   })
-
-
-  ### 如果直接在Seurat4中使用的话会报错，先转为稠密矩阵，再转dgCMatrix############
-  # Error in as(object = x, Class = "dgCMatrix") :
-  #   no method or default for coercing “dgRMatrix” to “dgCMatrix”
 
   mtx.data %<>% as.matrix %>% as("dgCMatrix")
   mtx.count %<>% as.matrix %>% as("dgCMatrix")
